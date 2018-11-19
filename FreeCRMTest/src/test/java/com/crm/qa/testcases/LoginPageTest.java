a4284237b20667d56293511c2994ea554041a964
Admin <Admin@Admin-PC>
2018-11-19 15:45:57 +0530

    test

--- FreeCRMTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -26,7 +26,7 @@ public class LoginPageTest extends TestBase{
 	@Test(priority =1)
 		public void loginPageTitleTest() {
 		String title = loginPage.validateLoginPageTitle();
-		Assert.assertEquals(title, "#1 Free CRM software in the cloud for sales and service");
+		Assert.assertEquals(title, "#1 Free CRM software in the cloud for sales and service", "Expected Title is not found");
 	}
 	
 	@Test(priority=2)
