64e4fcdc20a846b87c0e4108440f29bd773adba1
Admin <Admin@Admin-PC>
2018-11-21 10:25:40 +0530

    tet

--- FreeCRMTest/src/main/java/com/crm/qa/ExtentReportListner/ExtentReporterNG.java
+++ FreeCRMTest/src/main/java/com/crm/qa/ExtentReportListner/ExtentReporterNG.java
@@ -20,10 +20,11 @@ import com.relevantcodes.extentreports.LogStatus;
 public class ExtentReporterNG implements IReporter {
 	private ExtentReports extent;
 
+	@Override
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
 			String outputDirectory) {
 		extent = new ExtentReports(outputDirectory + File.separator
-				+ "Extent.html", true);
+				+ "Extent1.html", true);
 
 		for (ISuite suite : suites) {
 			Map<String, ISuiteResult> result = suite.getResults();
--- FreeCRMTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -1,14 +1,14 @@
 package com.crm.qa.testcases;
 
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-
 import java.util.concurrent.TimeUnit;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
